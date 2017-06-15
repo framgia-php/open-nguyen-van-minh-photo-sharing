95dc22f5584be33b5b149c3da68d91445ea6cd6a
minhnv2306 <minhnv2306@gmail.com>
2017-06-15 14:37:43 +0700

    Initialize migrations

--- project1/database/migrations/2017_05_28_030625_create_comment_table.php
+++ project1/database/migrations/2017_05_28_030625_create_comment_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCommentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::defaultStringLength(191);
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+        //
+        Schema::create('comment', function (Blueprint $table){
+            $table->integer('user_id');
+            $table->integer('image_id');
+            $table->string('comment',200);
             $table->timestamps();
         });
     }
@@ -31,6 +29,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
+        Schema::dropIfExists('comment');
     }
 }
