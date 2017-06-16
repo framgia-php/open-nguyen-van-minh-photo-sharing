26d1f07876ec1d6b6db615077016d0a8cb8c7300
minhnv2306 <minhnv2306@gmail.com>
2017-06-16 10:05:53 +0700

    Initialize Models

---
+++ project1/app/Http/Models/Image.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $primaryKey = 'image_id';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = array(
+        'image_id',
+        'scope',
+        'path',
+        'description',
+        'like',
+        'user_id',
+        'created_at',
+        'updated_at'
+    );
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+    public $timestamps = true;
+}
